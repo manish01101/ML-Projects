e9fa60fe2953a7ccee43cabe930e6f900d96b045
manish01101 <manishchotu4@gmail.com>
2025-10-29 08:37:03 +0530

    implemented backend of potato_disease_classification

---
+++ potato_disease_classification/api/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, File, UploadFile
+import numpy as np
+from io import BytesIO
+import tensorflow as tf
+from PIL import Image
+import uvicorn
+
+app = FastAPI()
+
+# Load model
+MODEL = tf.keras.models.load_model("../1.keras")
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
+@app.get("/hi")
+async def ping():
+    return {"message": "hi manish"}
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image = read_file_as_image(await file.read())
+
+    #convert to batch (model expects batch input)
+    img_batch = np.expand_dims(image, 0)
+
+    predictions = MODEL.predict(img_batch)
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+
+    return {
+        "class": predicted_class,
+        "confidence": float(confidence)
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
